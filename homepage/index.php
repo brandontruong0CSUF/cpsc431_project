f9469f2d9dcec037d1776d57aaa5096bef3071e1
Brandon Truong <brandontruong0@csu.fullerton.edu>
2015-05-07 00:12:02 -0700

    Implement angular

--- homepage/index.php
+++ homepage/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="en" data-ng-app="myApp">
 
   <head>
     <meta charset="utf-8">
@@ -13,28 +13,16 @@
 
   <body>
 
-    <div class="container">
-
-      <form class="form-signin" action="login.php" method="POST">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-success btn-block" type="submit">Sign in</button>
-      </form>
-    <!--
-      <form class="form-signin" method="POST" action="register.php">
-        <h3> Or register as a student </h3>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-      </form>
-    -->
-    </div>
+    <div id="view" data-ng-view></div>
 
     <!-- Load these after page is finished downloading -->
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="js/jquery.js"></script>
+		<script src="js/validator.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0-rc.1/angular.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/controllers.js"></script>
   </body>
 </html>
 
