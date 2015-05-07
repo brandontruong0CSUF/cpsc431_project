fc93fe7e1499e80da88a5a1e83ad7e7eca8b768a
Brandon Truong <brandontruong0@csu.fullerton.edu>
2015-05-07 00:11:47 -0700

    Add routes and title service

---
+++ homepage/js/app.js
@@ -0,0 +1,28 @@
+var myApp = angular.module("myApp", ["ngRoute", "myAppControllers"]);
+
+// Change page title when changing views
+myApp.factory("Page", function () {
+  var title = "Course Management System";
+  return {
+    title: function () { return title; },
+    setTitle: function (newTitle) { title = newTitle; }
+  }
+});
+
+// Routes
+myApp.config(["$routeProvider", function($routeProvider) {
+  var viewDir = "partials/"
+  $routeProvider
+    .when("/", {
+      templateUrl: viewDir + "login.html",
+      controller: "LoginCtrl"
+    }),
+		.when("/faculty", {
+      templateUrl: viewDir + "faculty.html",
+      controller: "FacultyCtrl"
+    }),
+		.when("/students", {
+      templateUrl: viewDir + "students.html",
+      controller: "StudentsCtrl"
+    });
+}]);
